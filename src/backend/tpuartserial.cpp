4435c40dc2c35d5f258e9d35edfe5878db0bbf1b
Matthias Urlichs <matthias@urlichs.de>
2015-04-25 12:48:30 +0200

    Pass "Trace *" parameter to all decoders
    so that we can add "tell me what's broken about this packet" logging

--- src/backend/tpuartserial.cpp
+++ src/backend/tpuartserial.cpp
@@ -221,7 +221,7 @@ TPUARTSerialLayer2Driver::Send_Queue_Empty ()
 void
 TPUARTSerialLayer2Driver::Send_L_Data (LPDU * l)
 {
-  TRACEPRINTF (t, 2, this, "Send %s", l->Decode ()());
+  TRACEPRINTF (t, 2, this, "Send %s", l->Decode (t)());
   inqueue.put (l);
   pth_sem_inc (&in_signal, 1);
 }
@@ -241,7 +241,7 @@ TPUARTSerialLayer2Driver::Get_L_Data (pth_event_t stop)
     {
       pth_sem_dec (&out_signal);
       LPDU *l = outqueue.get ();
-      TRACEPRINTF (t, 2, this, "Recv %s", l->Decode ()());
+      TRACEPRINTF (t, 2, this, "Recv %s", l->Decode (t)());
       return l;
     }
   else
@@ -299,7 +299,7 @@ TPUARTSerialLayer2Driver::RecvLPDU (const uchar * data, int len)
     }
   if (!mode)
     {
-      LPDU *l = LPDU::fromPacket (CArray (data, len));
+      LPDU *l = LPDU::fromPacket (CArray (data, len), t);
       if (l->getType () == L_Data && ((L_Data_PDU *) l)->valid_checksum)
 	{
 	  outqueue.put (l);
